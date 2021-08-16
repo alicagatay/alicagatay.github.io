1efb9453f41c7aca5a312edf9951d9f3a97c30b1
Ali Cagatay <alicagatay@Device.local>
2021-08-16 15:59:32 +0300

    Website remake.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,16 +3,16 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "9e1cf155a8bd45c6ddf1f83a7bd90ed8",
-"index.html": "5980e1b61e6bec8b968f7641a28945b7",
-"/": "5980e1b61e6bec8b968f7641a28945b7",
-"main.dart.js": "9d54d5aa806b84bcbe48b04b22b16062",
+  "version.json": "8d8372c5e85642e9a506c7bfe695d943",
+"index.html": "6a932e61a0f782d36fd033d03346eeb8",
+"/": "6a932e61a0f782d36fd033d03346eeb8",
+"main.dart.js": "030b2c45d49e121e8f8fd0d2922b4856",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"manifest.json": "ddbb8c8efa7742dc5d0dc7a2bc099a17",
+"manifest.json": "8c374db9428c1d0d8173f81497a552f1",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "bde9ee889e7cdb2c908965b81d635c52",
+"assets/NOTICES": "e2196c06ae480e6c5092faa91e1e996e",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1"
