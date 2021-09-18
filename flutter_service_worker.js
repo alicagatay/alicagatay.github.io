3efe5d6564a865d8daedc8efb2d7c1ad81b25f3c
alicagatay <aliccagatay@gmail.com>
2021-09-18 23:01:16 +0100

    Made some bug fixes.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,15 +4,17 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "8d8372c5e85642e9a506c7bfe695d943",
-"index.html": "f32463bde32ba979a6e71f87e21d85b2",
-"/": "f32463bde32ba979a6e71f87e21d85b2",
-"main.dart.js": "647d90d677f7e9862ccbb18b0fa61510",
+"index.html": "dea6507d9d77f39c1fab777357b67fc1",
+"/": "dea6507d9d77f39c1fab777357b67fc1",
+"main.dart.js": "c039bae1564e5801315da2b928f035b0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"manifest.json": "8c374db9428c1d0d8173f81497a552f1",
+"manifest.json": "f55c4df7a2a70065625b4e3c1f884bb2",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "a9a0b6f963ace5b79990857215ac81b4",
+"assets/NOTICES": "73f1f4bf5c9de8f5e30fa78196fad096",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1"
